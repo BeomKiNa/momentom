6656bd0304e6608743725c92b6c6302ec4a069c6
nabeomki <beomki69@gmail.com>
2021-11-23 02:43:21 +0900

    refactor: Change promise to async-await

--- src/js/Weather.js
+++ src/js/Weather.js
@@ -1,13 +1,7 @@
 import { COORDS } from "./constants.js";
 
 class Weather {
-  API_KEY = "8fe1baf3aef0a2073f16346b8f874d78";
   $weather = null;
-  weather = {
-    place: null,
-    sky: null,
-    temp: null,
-  };
 
   constructor({ $target }) {
     this.$weather = document.createElement("span");
@@ -18,39 +12,28 @@ class Weather {
     this.loadCoords();
   }
 
-  getWeather() {
+  async getWeather() {
     const {
       coordsObj: { latitude: lat, longitude: lng },
-      render,
     } = this;
 
-    fetch(
+    const response = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${process.env.API_KEY}&units=metric`
-    )
-      .then((response) => response.json())
-      .then((json) => {
-        console.log(json);
-        const {
-          name: place,
-          main: { temp },
-          weather,
-        } = json;
-        const sky = weather[0].main;
+    );
+    const {
+      name: place,
+      main: { temp },
+      weather,
+    } = await response.json();
+    const sky = weather[0].main;
 
-        this.weather = {
-          place,
-          sky,
-          temp,
-        };
-        render();
-      });
+    this.render({ place, sky, temp });
   }
 
   handleGeoSucces(position) {
     const {
       coords: { latitude, longitude },
     } = position;
-
     this.setState({ latitude, longitude });
   }
 
@@ -68,26 +51,22 @@ class Weather {
   }
 
   loadCoords() {
-    const { askForCoords, setState } = this;
     const getCoords = JSON.parse(localStorage.getItem(COORDS)) || null;
     if (getCoords === null) {
-      askForCoords();
+      this.askForCoords();
     } else {
-      setState(getCoords);
+      this.setState(getCoords);
     }
   }
 
-  async setState(coordsObj) {
+  setState(coordsObj) {
     this.coordsObj = coordsObj;
     this.saveCoords();
     this.getWeather();
   }
 
-  render() {
-    const {
-      $weather,
-      weather: { place, sky, temp },
-    } = this;
+  render({ place, sky, temp }) {
+    const { $weather } = this;
 
     if (place && sky && temp) {
       $weather.innerText = `${place} ${sky} ${temp}°C `;
