06b62b0844f5159efcca87e3d00f7906f6b4815a
nabeomki <beomki69@gmail.com>
2021-12-22 23:49:41 +0900

    build: Change webpack.config.js for TS

--- webpack.config.js
+++ webpack.config.js
@@ -9,10 +9,10 @@ const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   name: "momentom-setting",
-  mode: "production",
+  mode: "development",
   devtool: "hidden-source-map",
   resolve: {
-    extensions: [".js"],
+    extensions: [".js", ".ts"],
   },
   entry: {
     app: "./src/js/index",
@@ -21,10 +21,10 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.(js|ts)$/,
         loader: "babel-loader",
         options: {
-          presets: ["@babel/preset-env"],
+          presets: ["@babel/preset-env", "@babel/preset-typescript"],
         },
       },
       {
