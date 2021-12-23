65efe232494c641d0f9111805aaf1a45d9cabe6c
nabeomki <beomki69@gmail.com>
2021-12-24 00:00:09 +0900

    build: ts-loader가 아닌 babel-loader를 사용하기 위해 @babel/preset-env 를 @babel/preset-typescript 로 수정

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
         test: /\.(js|ts)$/,
         loader: "babel-loader",
         options: {
-          presets: ["@babel/preset-env", "@babel/preset-typescript"],
+          presets: ["@babel/preset-typescript"],
         },
       },
       {
