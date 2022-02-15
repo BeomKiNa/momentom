b66f522699dbf5693c917c3115646113d1fb273d
nabeomki <beomki69@gmail.com>
2022-02-15 22:03:53 +0900

    refactor: Content.js를 ts 파일로 변경

---
+++ src/ts/Content.ts
@@ -0,0 +1,27 @@
+import Clock from "./Clock";
+import TasksContainer from "./TasksContainer";
+import UserName from "./userName";
+import Weather from "./weather";
+
+class Content {
+  private $target: HTMLElement;
+  private $content: HTMLElement;
+
+  constructor({ $target }) {
+    this.$target = $target;
+    this.$content = document.createElement("section");
+    this.$content.className = "content";
+
+    const weather = new Weather({ $target: this.$content });
+
+    const clock = new Clock({ $target: this.$content });
+
+    const userName = new UserName({ $target: this.$content });
+
+    const tasksContainer = new TasksContainer({ $target: this.$content });
+
+    this.$target.appendChild(this.$content);
+  }
+}
+
+export default Content;
